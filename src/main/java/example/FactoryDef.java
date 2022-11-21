97622372a67c0eeda3415363af0afa552ff9a37a
Marcin Majewski <69035061+marcin-majewski-sonarsource@users.noreply.github.com>
2022-11-21 15:03:09 +0100

    In-code set up

--- src/main/java/example/FactoryDef.java
+++ src/main/java/example/FactoryDef.java
@@ -1,8 +1,11 @@
 package example;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import io.micronaut.context.annotation.Factory;
 import jakarta.inject.Singleton;
 import javax.sql.DataSource;
+import org.apache.ibatis.datasource.DataSourceFactory;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -13,14 +16,18 @@ import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 @Factory
 public class FactoryDef {
 
-  private final DataSource dataSource;
-
-  public FactoryDef(DataSource dataSource) {
-    this.dataSource = dataSource;
+  @Singleton
+  public DataSource dataSource() {
+    HikariConfig hikariConfig = new HikariConfig();
+    hikariConfig.setDriverClassName("org.postgresql.Driver");
+    hikariConfig.setUsername("<username>");
+    hikariConfig.setPassword("<password>");
+    hikariConfig.setJdbcUrl("jdbc:postgresql://localhost:5432/<db>");
+    return new HikariDataSource(hikariConfig);
   }
 
   @Singleton
-  public SqlSessionFactory sqlSessionFactory() {
+  public SqlSessionFactory sqlSessionFactory(DataSource dataSource) {
     TransactionFactory transactionFactory = new JdbcTransactionFactory();
 
     Environment environment = new Environment("dev", transactionFactory, dataSource);
